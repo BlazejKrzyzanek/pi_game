b637cf2980e36e5bdae8d82fcd98ae87da038d16
BlazejKrzyzanek <bl.krzyzanek@gmail.com>
2020-01-21 10:48:16 +0100

    .

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
-#! /usr/bin/env python
+# import modules
+import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)  # choose BCM or BOARD numbering schemes. I use BCM
-GPIO.setup(25, GPIO.OUT)# set GPIO 25 as an output. You can use any GPIO port
-p = GPIO.PWM(25, 50)    # create an object p for PWM on port 25 at 50 Hertz
-p.start(70)             # start the PWM on 70 percent duty cycle
- for x in range(200, 2200):
-      p.ChangeFrequency(x)  # change the frequency to x Hz (
-      time.sleep(0.0001)
-p.stop()                # stop the PWM output
-GPIO.cleanup()          # when your program exits, tidy up after yourself
+import time
+
+# setup pins
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(5, GPIO.IN)
+
+# loop 5 times
+for i in range(5):
+
+    # flash output pin 3
+    GPIO.output(3, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(3, GPIO.LOW)
+    time.sleep(1)
+
+    # read input pin 5
+    if GPIO.input(5) == GPIO.HIGH:
+        print("Pin 5 is on")
+    else:
+        print("Pin 5 is off")
